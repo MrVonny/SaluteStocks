d33ea6d29753e353cf367f1b5997e9b50a096782
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-02-16 04:54:33 +0300

    inital screener

---
+++ SaluteStocksAPI/Service/IDataBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using SaluteStocksAPI.Models.FundamentalData;
+using SaluteStocksAPI.Screener;
+
+namespace SaluteStocksAPI.Service;
+
+public interface IDataBaseRepository
+{
+    public Task SetListing(object listing);
+    public Task<List<string>> GetListing();
+    public Task<List<Tuple<string, DateTime>>> GetListingWithLastUpdateDate();
+
+
+    public Task SetCompanyOverview(CompanyOverview companyOverview);
+}
--- SaluteStocksAPI/Service/Loader.cs
+++ SaluteStocksAPI/Service/Loader.cs
@@ -1,6 +1,31 @@
-﻿namespace SaluteStocksAPI.Service;
+﻿using System.Collections.Concurrent;
+using SaluteStocksAPI.AlphaVantage;
+
+namespace SaluteStocksAPI.Service;
 
 public class Loader
 {
-    
+    private readonly IDataBaseRepository _repository;
+    private AlphaVantageClient Client => AlphaVantageClientFactory.Create();
+    private ConcurrentBag<Tuple<string, DateTime>> SymbolList { get; set; }
+
+    public Loader()
+    {
+        SymbolList = new ConcurrentBag<Tuple<string, DateTime>>(_repository.GetListingWithLastUpdateDate().Result);
+        
+    }
+
+    public void Do()
+    {
+        while (true)
+        {
+            Task.Run(() =>
+            {
+                var symbol = SymbolList.MinBy(x => x.Item2)?.Item1;
+                var companyOverview = Client.GetCompanyOverview(symbol).Result;
+                _repository.SetCompanyOverview(companyOverview);
+            });
+            Thread.Sleep(100);
+        }
+    }
 }
