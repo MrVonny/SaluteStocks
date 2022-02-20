dd62d4b3af23b859deb43db52d4c033654063977
nfrmtk <Lykov.ivan.02@mail.ru>
2022-02-20 20:40:12 +0300

    Added afew tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using SaluteStocksAPI.AlphaVantage;
 using SaluteStocksAPI.AlphaVantage.Common;
 using SaluteStocksAPI.Models.Core;
+using SaluteStocksAPI.Models.FundamentalData;
 
 namespace TestProject1;
 
@@ -20,14 +21,6 @@ namespace TestProject1;
     }
 
     [Test]
-    public async Task Test1()
-    {
-        var timeSeries = await Client.GetTimeSeriesDaily("IBM", adjusted:false);
-        
-        Assert.AreEqual( "130.4400",timeSeries[0].High);
-    }
-
-    [Test]
     public async Task GetListingTest()
     {
         var listing = await Client.GetListing();
@@ -36,4 +29,49 @@ namespace TestProject1;
         Assert.That(listing, Is.All.Not.Null);
         Assert.That(listing.Select(x=>x.Symbol), Is.All.Not.Null);
     }
+
+    [Test]
+    public async Task GetTimeSeriesDailyTest()
+    {
+        var timeSeriesCsv = await Client.GetTimeSeriesDailyCsv("IBM", adjusted:false );
+        
+        Assert.Greater(timeSeriesCsv.Count, 100);
+        Assert.That(timeSeriesCsv, Is.All.Not.Null);
+
+        var timeSeriesJson = await Client.GetTimeSeriesDailyJson("IBM", adjusted: false);
+        
+        Assert.Greater(timeSeriesJson.MonthlyTimeSeries.Count,  10);
+        Assert.That(timeSeriesJson.MonthlyTimeSeries, Is.All.Not.Null);
+        var checkDate = DateOnly.FromDateTime( DateTime.Today.AddDays(1));
+        Assert.Equals(timeSeriesCsv.Select(x => x.TimeStamp = checkDate),
+            timeSeriesJson.MonthlyTimeSeries.Select(x => x.TimeStamp == checkDate));
+
+    }
+    
+    [Test] public async Task GetCompanyEarningsTest()
+    {
+        Earnings? companyEarnings = await Client.GetCompanyEarnings("IBM");
+        Assert.That(companyEarnings.AnnualEarnings, Is.All.Not.Null);
+    }
+    
+    
+    [Test] public async Task GetCompanyCashFlowTest()
+    {
+        var companyCashFlow = await Client.GetCashFlow("IBM");
+        Assert.That(companyCashFlow.AnnualReports, Is.All.Not.Null);
+    }
+    
+    [Test] public async Task GetCompanyBalanceSheetTest()
+    {
+        var companyBalanceSheet = await Client.GetBalanceSheet("IBM");
+        Assert.That(companyBalanceSheet.AnnualReports, Is.All.Not.Null);
+    }
+
+    [Test]
+    public async Task GetCompanyIncomeStatement()
+    {
+        var companyIncomeStatement = await Client.GetIncomeStatement("IBM");
+        Assert.That(companyIncomeStatement.AnnualReports, Is.All.Not.Null );
+    }
+    
 }
