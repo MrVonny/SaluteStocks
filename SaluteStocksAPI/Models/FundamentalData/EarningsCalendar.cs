4881f3d64b4f6162925c5564eecf470c22ac0954
Leminodia Leminodia <Lykov.ivan.02@yandex.ru>
2022-02-23 13:05:08 +0300

    restroed 4 classes supporting previos functions

---
+++ SaluteStocksAPI/Models/FundamentalData/EarningsCalendar.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using SaluteStocksAPI.Models.FundamentalData.Common;
+
+namespace SaluteStocksAPI.Models.FundamentalData;
+
+public class EarningsCalendar : Report
+{
+    [JsonProperty("symbol")] public string Symbol { get; set; }
+    
+    [JsonProperty("name")] public string Name { get; set; }
+    
+    [JsonProperty("reportDate")] public DateTime? ReportDate { get; set; }
+    
+    [JsonProperty("estimate")] public double? Estimate { get; set; }
+
+
+}
