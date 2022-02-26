64d27002d6412baf36d3619e7900d24e6882f955
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-02-26 23:25:18 +0300

    Add Serilog

--- SaluteStocksAPI/Program.cs
+++ SaluteStocksAPI/Program.cs
@@ -1,50 +1,60 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SaluteStocksAPI.DataBase;
 using SaluteStocksAPI.Service;
 using SaluteStocksAPI.Service.Background;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+    // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new() { Title = "SaluteStocksAPI", Version = "v1" }); });
+    builder.Services.AddControllers();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "SaluteStocksAPI", Version = "v1" });
+    });
 
-//DB Context
-builder.Services.AddDbContext<StocksContext>(
-    options => options.UseMySql(builder.Configuration.GetConnectionString("MySql"),
-    MySqlServerVersion.LatestSupportedServerVersion));
+    //DB Context
+    builder.Services.AddDbContext<StocksContext>(options =>
+        options.UseMySql(builder.Configuration.GetConnectionString("MySql"),
+            MySqlServerVersion.LatestSupportedServerVersion));
 
-//DB repository
-builder.Services.AddScoped<IDataBaseRepository, DataBaseRepository>();
-//Screener
-builder.Services.AddScoped<ScreenerService>();
-//Loader background service
-builder.Services.AddHostedService(provider => new Loader(
-        new LoaderSettings
-        {
-            CheckUpdateTime = TimeSpan.FromSeconds(2)
-        },
-        provider.GetService<IServiceScopeFactory>()
-    )
-);
+    //DB repository
+    builder.Services.AddScoped<IDataBaseRepository, DataBaseRepository>();
+    //Screener
+    builder.Services.AddScoped<ScreenerService>();
+    //Loader background service
+    builder.Services.AddHostedService(provider => new Loader(
+        new LoaderSettings { CheckUpdateTime = TimeSpan.FromSeconds(2) }, provider.GetService<IServiceScopeFactory>()));
 
-
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (builder.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SaluteStocksAPI v1"));
+    if (builder.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SaluteStocksAPI v1"));
+    }
+
+    app.UseHttpsRedirection();
+
+    //app.UseAuthorization();
+
+    app.MapControllers();
+    app.Run();
+    Log.Information("Stopped cleanly");
+    return 0;
 }
-
-
-app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
