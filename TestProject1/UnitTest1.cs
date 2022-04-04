b7cf97c4e0c4350c10563a76c9b0d3fad3aa9e4c
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-04-04 23:09:25 +0300

    test base

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EFCore.BulkExtensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using SaluteStocksAPI.AlphaVantage;
 using SaluteStocksAPI.DataBase;
@@ -11,14 +14,33 @@ using SaluteStocksAPI.Service;
 
 namespace TestProject1;
 
-[TestFixture] public class Tests
+[TestFixture]
+public class BaseTest
 {
+    protected StocksContext StocksContext = null;
     protected AlphaVantageClient Client = AlphaVantageClientFactory.Create();
+
     [SetUp]
-    public void Setup()
+    public virtual void SetUp()
     {
-        
+        var optionsBuilder = new DbContextOptionsBuilder<StocksContext>();
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        string connectionString = config.GetConnectionString("MSSQL");
+        var options = optionsBuilder
+            .UseSqlServer(connectionString)
+            .Options;
+
+        StocksContext = new StocksContext(options);
     }
+}
+[TestFixture]
+public class Tests : BaseTest
+{
+    
 
     [Test]
     public async Task GetSearchResults()
@@ -127,10 +149,16 @@ namespace TestProject1;
     }
 
     
-    [Test]public async Task WhereEPSTest()
+    [Test]
+    public async Task WhereEPSTest()
     {
-        var screenerService = new ScreenerService(new StocksContext(new DbContextOptions<StocksContext>()));
-        var symbolList = await screenerService.GetStockSymbols(new ScreenerModel());
-        Assert.That( symbolList, Is.Not.Empty);
+        List<string> symbolList;
+        await using (StocksContext)
+        {
+            var screenerService = new ScreenerService(StocksContext);
+            symbolList = await screenerService.GetStockSymbols(new ScreenerModel());
+            Assert.That( symbolList, Is.Not.Empty);
+            Assert.Greater(symbolList.Count, 500);
+        }
     }
 }
